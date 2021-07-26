89e97e41a3eef44a997f40f62f46e585c3d3b920
SeungjaeKim <kimseungjae@gmail.com>
2021-07-26 10:12:59 +0900

    Initial commit

---
+++ src/test/java/kr/co/codeplus/CodePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.codeplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
